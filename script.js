b320eb507f2cf01cfca34f83f64286a7378f22ef
Somia Sohail Ansari <48806685+somia-ansari@users.noreply.github.com>
2019-06-13 21:03:43 +0500

    WIP-JS

---
+++ script.js
@@ -0,0 +1,13 @@
+let banana= document.querySelector ("#banana-number");
+let apple= document.querySelector ("#apple-number");
+let orange= document.querySelector ("#orange-number");
+
+function addBanana () {
+    banana.value = (+1);
+}
+function addApple () {
+    apple.value = (+1);
+}
+function addOrange () {
+    orange.value = (+1);
+}
