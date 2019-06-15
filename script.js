8ee610dc03c4c2dc25434013775ba71d13b6e4f4
Somia Sohail Ansari <48806685+somia-ansari@users.noreply.github.com>
2019-06-16 00:29:45 +0500

    WIP- JS of making bills
    decreasing & increasing input value is done

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-let banana= document.querySelector ("#banana-number");
-let apple= document.querySelector ("#apple-number");
-let orange= document.querySelector ("#orange-number");
+let totalBill = document.querySelector ('#bill input').value;
+
+// For buying bananas
 
 function addBanana () {
-    banana.value = (+1);
+    var banana = parseInt(document.querySelector('#banana-number').value);
+    banana++;
+
+    document.querySelector('#banana-number').value = banana;
 }
+
+function minusBanana() {
+    var banana = parseInt(document.querySelector('#banana-number').value);
+    banana < 1? banana = 1: '';
+    banana--;
+
+    document.querySelector('#banana-number').value = banana;
+}
+// For buying apples
 function addApple () {
-    apple.value = (+1);
+    var apple = parseInt(document.querySelector('#apple-number').value);
+    apple++;
+
+    document.querySelector('#apple-number').value = apple;
 }
+function minusApple() {
+    var apple = parseInt(document.querySelector('#apple-number').value);
+    apple < 1? apple = 1: '';
+    apple--;
+
+    document.querySelector('#apple-number').value = apple;
+}
+// For buying oranges
+
 function addOrange () {
-    orange.value = (+1);
+    var orange = parseInt(document.querySelector('#orange-number').value);
+    orange++;
+
+    document.querySelector('#orange-number').value = orange;
 }
+function minusOrange() {
+    var orange = parseInt(document.querySelector('#orange-number').value);
+    orange < 1? orange = 1: '';
+    orange--;
+
+    document.querySelector('#orange-number').value = orange;
+}
+// For making bill of oranges
+    let costOfOrange = document.querySelector(orange);
