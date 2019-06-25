48cf2add4527780eeb0ba522fad9af9b10b3fc7e
Somia Sohail Ansari <48806685+somia-ansari@users.noreply.github.com>
2019-06-25 15:29:07 +0500

    removed typo in functions
    WIP- Bill alert

--- script.js
+++ script.js
@@ -1,65 +1,3 @@
-
-// For buying bananas
-let banana = 0;
-function addBanana () {
-    let pocket = document.querySelector('#pocket input').value;
-    let bill = document.querySelector('#bill input').value;
-
-    if (pocket <= bill ) {
-        alert("You have not enough money to buy Bananas");
-    }
-    else {
-        document.querySelector('#banana-number').value = ++banana;
-        updateBill();
-    }
-    
-}
-function minusBanana() {
-    banana < 1? banana = 1: '';
-    document.querySelector('#banana-number').value = --banana;
-    updateBill();
-}
-// For buying apples
-function addApple () {
-
-let pocket = document.querySelector('#pocket input').value;
-let bill = document.querySelector('#bill input').value;
-
-    if (pocket <= bill ) {
-        alert("You have not enough money to buy Apple");
-    }
-    else {
-        document.querySelector('#apple-number').value = ++apples;
-        updateBill();
-    }
-}
-let apples = 0;
-function minusApple() {
-    apples < 1? apples = 1: '';
-    document.querySelector('#apple-number').value = --apples;   
-updateBill();
-}
-// For buying oranges
-
-function addOrange () {
-    let pocket = document.querySelector('#pocket input').value;
-    let bill = document.querySelector('#bill input').value;
-    
-        if (pocket <= bill ) {
-            alert("You have not enough money to buy Orange");
-        }
-        else {
-            document.querySelector('#orange-number').value = ++orange;
-            updateBill();
-        }
-}
-let orange = 0;
-function minusOrange() {
-    orange < 1? orange = 1: '';
-
-    document.querySelector('#orange-number').value = --orange;
-    updateBill();
-}
 // For making bills
 
 function updateBill() {
@@ -67,9 +5,71 @@ function updateBill() {
     const totalOrangePrice = orange * 15;
     const totalBananaPrice = banana * 7;
     let bill = document.querySelector('#bill input').value;
-    
+
     bill = totalApplePrice + totalOrangePrice + totalBananaPrice;
     document.querySelector('#bill input').value = bill;
-  }
-  
+}
+// For buying apples
 
+
+function addApple() {
+
+    let pocket = document.querySelector('#pocket input').value;
+    let bill = document.querySelector('#bill input').value;
+
+    if (pocket <= bill) {
+        alert("You have not enough money to buy Apple");
+    } else {
+        document.querySelector('#apple-number').value = ++apples;
+        updateBill();
+    }
+}
+let apples = 0;
+
+function minusApple() {
+    apples < 1 ? apples = 1 : '';
+    document.querySelector('#apple-number').value = --apples;
+    updateBill();
+}
+// For buying oranges
+
+function addOrange() {
+    let pocket = document.querySelector('#pocket input').value;
+    let bill = document.querySelector('#bill input').value;
+
+    if (pocket <= bill) {
+        alert("You have not enough money to buy Orange");
+    } else {
+        document.querySelector('#orange-number').value = ++orange;
+        updateBill();
+    }
+}
+let orange = 0;
+
+function minusOrange() {
+    orange < 1 ? orange = 1 : '';
+
+    document.querySelector('#orange-number').value = --orange;
+    updateBill();
+}
+// For buying bananas
+let banana = 0;
+
+function addBanana() {
+    let pocket = document.querySelector('#pocket input').value;
+    let bill = document.querySelector('#bill input').value;
+
+    if (pocket <= bill) {
+        alert("You have not enough money to buy Bananas");
+    } else {
+        document.querySelector('#banana-number').value = ++banana;
+        updateBill();
+    }
+
+}
+
+function minusBanana() {
+    banana < 1 ? banana = 1 : '';
+    document.querySelector('#banana-number').value = --banana;
+    updateBill();
+}
